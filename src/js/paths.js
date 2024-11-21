7018cd6178dd13800e971319dc599f55f9a0f19a
Mehmox <mehmetugurlu97@icloud.com>
2024-11-22 01:29:17 +0300

    repair

---
+++ src/js/paths.js
@@ -0,0 +1,21 @@
+const targetPaths = [
+    "backpack_icons",
+    "quiver", "talisman_bag",
+    "equipment_contents",
+    "inv_armor",
+    "inv_contents",
+    "wardrobe_contents"
+]
+export default targetPaths
+/*
+"backpack_contents",
+    "backpack_icons",
+    "bag_contents.fishing_bag", "bag_contents.potion_bag", "bag_contents.quiver", "bag_contents.sacks_bag", "bag_contents.talisman_bag",
+    "ender_chest_contents",
+    "equipment_contents",
+    "inv_armor",
+    "inv_contents",
+    "personal_vault_contents",
+    "sacks_counts",
+    "wardrobe_contents"
+*/
